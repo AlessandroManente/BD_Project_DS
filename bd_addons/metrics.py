89b629e5e27b42e08189fa6aa17a892d9a0a869b
riccardomazzieri <riccardo.mazzieri@gmail.com>
2021-02-10 20:40:16 +0100

    Actually modified the parser

--- bd_addons/metrics.py
+++ bd_addons/metrics.py
@@ -1,14 +1,8 @@
 from Bio import SeqIO, SearchIO
 import pandas as pd
 import os
-from bd_addons.HmmPy import *
-import time
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
-import random
-sns.set_theme()
-sns.set_style("whitegrid")
+from bd_addons.HmmPy import *
 
 cur = os.getcwd()
 path_swiss_prot = cur + '\\data_team_1\\swiss_prot\\uniprot_sprot.fasta'
@@ -48,6 +42,7 @@ def parse_psiblast():
 
 def psiblast_parser(dir_to_parse, filename, extension, dir_parsed):   
     if filename + '.csv' not in os.listdir(dir_parsed): 
+        print("Parsing {} ...".format(filename))
         hits_dict = {}
         blast_records = SearchIO.parse(dir_to_parse + '\\' + filename + '.' + extension, 'blast-xml')
         for blast_record in blast_records:
@@ -57,6 +52,13 @@ def psiblast_parser(dir_to_parse, filename, extension, dir_parsed):
                 hits_dict.setdefault("hit_start",[]).append(rec.hsps[0].hit_range[0]+1)
                 hits_dict.setdefault("hit_end",[]).append(rec.hsps[0].hit_range[1])
                 hits_dict.setdefault("bitscore",[]).append(rec.hsps[0].bitscore)
+
+        lengths = []
+        with open(dir_to_parse + '\\' + filename + '.' + extension, 'r') as f:
+            for line in f:
+                if(line[2:11] == '<Hit_len>'):
+                    lengths.append(line.split('>')[1].split('<')[0])
+        hits_dict["protein_length"] = lengths
             
         hits_df = pd.DataFrame.from_dict(hits_dict)
         hits_df.to_csv(dir_parsed + '\\' + filename + '.csv')
@@ -73,10 +75,12 @@ def metrics_sequences(df, gt):
     df_ids = df.ids.to_list()
     swissprot_df = swiss_prot_parser()
     
-    true_positives = (df['ids'].isin(gt_acc)).sum() # intersection between gt.ids and df.ids
-    true_negatives = ((~swissprot_df['ids'].isin(gt_acc)) & (~swissprot_df['ids'].isin(df_ids))).sum() # rows in swissprot_df but not in gt.ids and df.ids
+
+    true_positives = df['ids'].apply(lambda x: 1 if x in gt_acc else 0).sum() # intersection between gt.ids and df.ids
+    true_negatives = swissprot_df['ids'].apply(lambda x: 1 if (x not in gt_acc and x not in df_ids) else 0).sum() # rows in swissprot_df but not in gt.ids and df.ids
     false_positives = len(df) - true_positives
     false_negatives = len(swissprot_df) - len(df) - true_negatives#swissprot_df['ids'].apply(lambda x: 1 if x not in df_ids else 0).sum() - true_negatives
+    
     
     accuracy = (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
     
@@ -113,17 +117,3 @@ def metrics_8(gt):
     metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_8.csv')
 
     return metrics_df, parsed_tblouts, parsed_domtblouts, parsed_psiblast
-
-
-def plot_metrics_8(metrics_df):
-    col= list(metrics_df.columns)
-    x = np.arange(metrics_df.shape[0])
-    c = np.random.rand(metrics_df.shape[0],3)
-    for i in col:
-        fig, ax = plt.subplots(figsize=(20,10))
-        ax.bar(metrics_df.index,list(metrics_df[i]), color = c)
-        ax.set_xticks(x)
-        ax.set_xticklabels(metrics_df.index, rotation=65)
-        ax.set_ylim((0,1))
-        plt.title(i)
-        #fig.savefig(i+".png")
