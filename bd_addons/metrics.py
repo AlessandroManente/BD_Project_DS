5070ccee85c05b7c2cb5c7141a7a79042caa6bca
Nihmar <manente.alessandro@gmail.com>
2021-02-10 23:56:33 +0100

    Add (again) plots, add metrics_9

--- bd_addons/metrics.py
+++ bd_addons/metrics.py
@@ -1,7 +1,14 @@
 from Bio import SeqIO, SearchIO
 import pandas as pd
 import os
+from bd_addons.HmmPy import *
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+import random
+sns.set_theme()
+sns.set_style("whitegrid")
 from bd_addons.HmmPy import *
 
 cur = os.getcwd()
@@ -106,14 +113,262 @@ def metrics_8(gt):
     metrics = []
     index_metrics = list(parsed_domtblouts.keys()) + list(parsed_psiblast.keys())
     columns_metrics = ['accuracy', 'precision', 'recall', 'specificity', 'balanced_accuracy', 'mcc', 'f1_score']
+    
+    if 'metrics_8.csv' in os.listdir(cur + '\\data_team_1\\metrics'):
+        old_metrics_df = pd.read_csv(cur + '\\data_team_1\\metrics\\metrics_8.csv', index_col=0)
+
+        if old_metrics_df.index.to_list() == index_metrics:
+            return old_metrics_df, parsed_tblouts, parsed_domtblouts, parsed_psiblast
+        
+        else:
+            for df in parsed_domtblouts.keys():
+                metrics.append(metrics_sequences(parsed_domtblouts[df], gt))
+            
+            for df in parsed_psiblast.keys():
+                metrics.append(metrics_sequences(parsed_psiblast[df], gt))
+            
+            metrics_df = pd.DataFrame(metrics, index_metrics, columns_metrics)
+            metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_8.csv')
+
+            return metrics_df, parsed_tblouts, parsed_domtblouts, parsed_psiblast
+    
+    else:
+        for df in parsed_domtblouts.keys():
+            metrics.append(metrics_sequences(parsed_domtblouts[df], gt))
+            
+            for df in parsed_psiblast.keys():
+                metrics.append(metrics_sequences(parsed_psiblast[df], gt))
+            
+            metrics_df = pd.DataFrame(metrics, index_metrics, columns_metrics)
+            metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_8.csv')
+
+            return metrics_df, parsed_tblouts, parsed_domtblouts, parsed_psiblast
+
+
+def dfs_to_dicts(parsed_domtblouts, parsed_psiblast):
+    # from dfs to dicts with no repeated columns -> fast af
+    list_dfs = list(parsed_domtblouts.keys()) + list(parsed_psiblast.keys())
+    list_dfs_not_repeated = []
 
     for df in parsed_domtblouts.keys():
-        metrics.append(metrics_sequences(parsed_domtblouts[df], gt))
-    
-    for df in parsed_psiblast.keys():
-        metrics.append(metrics_sequences(parsed_psiblast[df], gt))
-    
-    metrics_df = pd.DataFrame(metrics, index_metrics, columns_metrics)
-    metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_8.csv')
+        df_not_repeated = {}
+        for i, row in parsed_domtblouts[df].iterrows():
+            if 'domtblout' in df:
+                df_not_repeated.setdefault(row['ids'],[]).append([row['from_ali_coord'], row['to_ali_coord'], row['target_len']])
+            else:
+                df_not_repeated.setdefault(row['ids'],[]).append([row['hit_start'], row['hit_end'], row['protein_length']])
+        
+        list_dfs_not_repeated.append(df_not_repeated)
 
-    return metrics_df, parsed_tblouts, parsed_domtblouts, parsed_psiblast
+    for df in parsed_psiblast.keys():
+        df_not_repeated = {}
+        for i, row in parsed_psiblast[df].iterrows():
+            if 'domtblout' in df:
+                df_not_repeated.setdefault(row['ids'],[]).append([row['from_ali_coord'], row['to_ali_coord'], row['target_len']])
+            else:
+                df_not_repeated.setdefault(row['ids'],[]).append([row['hit_start'], row['hit_end'], row['protein_length']])
+        
+        list_dfs_not_repeated.append(df_not_repeated)
+
+    dict_dfs = dict(zip(list_dfs, list_dfs_not_repeated))
+
+    return dict_dfs
+
+
+def new_logic_case_1(a, b):#, tp, tn, fp, fn):
+    global tp
+    global tn
+    global fp
+    global fn
+
+    if a == 0 and b == 1:
+        fp += 1
+    
+    elif a == 1 and b == 1:
+        tp += 1
+
+    elif a == 1 and b == 0:
+        fn += 1
+    
+    else:
+        tn += 1
+    
+
+def new_logic_case_2(a, b):#, tp_, tn_, fp_, fn_):
+    global tp_
+    global tn_
+    global fp_
+    global fn_
+
+    if a == 0 and b == 1:
+        fp_ += 1
+    
+    elif a == 1 and b == 1:
+        tp_ += 1
+
+    elif a == 1 and b == 0:
+        fn_ += 1
+    
+    else:
+        tn_ += 1
+
+
+def compute_con_matrix_9(parsed_domtblouts, parsed_psiblast, gt):
+    new_logic_case_1_vectorized = np.vectorize(new_logic_case_1)
+    new_logic_case_2_vectorized = np.vectorize(new_logic_case_2)
+
+    gt_acc = gt.accession.to_list()
+
+    dict_dfs = dfs_to_dicts(parsed_domtblouts, parsed_psiblast)
+
+    conf_matrix = []
+
+    #conf_df = pd.DataFrame(columns=['true_positives', 'true_negatives', 'false_positives', 'false_negatives'])
+    
+    for k, (df_name, df_dict) in enumerate(dict_dfs.items()):
+        gt_int_df = [x for x in gt_acc if x in list(df_dict.keys())]
+        df_not_gt = [x for x in list(df_dict.keys()) if x not in gt_acc]
+        gt_not_df = [x for x in gt_acc if x not in list(df_dict.keys())]
+
+        global tp
+        global tn
+        global fp
+        global fn
+        global tp_
+        global tn_
+        global fp_
+        global fn_
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
+        tp_ = 0
+        tn_ = 0
+        fp_ = 0
+        fn_ = 0
+
+        for j, (ids, lists) in enumerate(df_dict.items()):
+            if ids in gt_int_df:
+                row_df = np.zeros((int(lists[0][2]),), dtype=int)
+
+                for i, el in enumerate(lists):
+                    row_df[int(el[0]) : int(el[1])] = 1
+                
+                row_gt = np.zeros((int(lists[0][2]),), dtype=int)
+                row_gt[gt[gt.accession == ids].iloc[0,1] : gt[gt.accession == ids].iloc[0,2]] = 1
+                
+                new_logic_case_1_vectorized(row_gt, row_df)#, tp, tn, fp, fn)
+
+            elif ids in df_not_gt:
+                row_df = np.zeros((int(lists[0][2]),), dtype=int)
+
+                for i, el in enumerate(lists):
+                    row_df[int(el[0]) : int(el[1])] = 1
+                
+                row_gt = np.zeros((int(lists[0][2]),), dtype=int)
+                
+                new_logic_case_2_vectorized(row_gt, row_df)#, tp_, tn_, fp_, fn_)
+        
+        if gt_not_df != []:
+            for ids in gt_not_df:
+                row_gt = np.zeros((gt[gt.accession == ids].iloc[0,3],), dtype=int)
+                row_gt[gt[gt.accession == ids].iloc[0,1] : gt[gt.accession == ids].iloc[0,2]] = 1
+
+                row_df = np.zeros((gt[gt.accession == ids].iloc[0,3],), dtype=int)
+
+                new_logic_case_1_vectorized(row_gt, row_df)#, tp, tn, fp, fn)
+                
+        conf_matrix.append([tp + tp_, tn + tn_, fp + fp_, fn + fn_])
+    
+    #conf_df = pd.DataFrame(conf_matrix, columns=['true_positives', 'true_negatives', 'false_positives', 'false_negatives'])
+
+    conf_df = pd.DataFrame(conf_matrix, index=list(dict_dfs.keys()), columns=['true_positives', 'true_negatives', 'false_positives', 'false_negatives'])
+
+    return conf_df
+
+
+def metrics_9(parsed_domtblouts, parsed_psiblast, gt):
+    conf_df = compute_con_matrix_9(parsed_domtblouts, parsed_psiblast, gt)
+
+    index_metrics = list(parsed_domtblouts.keys()) + list(parsed_psiblast.keys())
+    columns_metrics = ['accuracy', 'precision', 'recall', 'specificity', 'balanced_accuracy', 'mcc', 'f1_score']
+
+    metrics_list = []
+
+    if 'metrics_9.csv' in os.listdir(cur + '\\data_team_1\\metrics'):
+        old_metrics_df = pd.read_csv(cur + '\\data_team_1\\metrics\\metrics_9.csv', index_col=0)
+
+        if old_metrics_df.index.to_list() == index_metrics:
+            return old_metrics_df, conf_df
+        
+        else:
+            for i, row in conf_df.iterrows():
+                true_positives = row['true_positives']
+                true_negatives = row['true_negatives']
+                false_positives = row['false_positives']
+                false_negatives = row['false_negatives']
+
+                accuracy = (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
+            
+                precision = true_positives / (true_positives + false_positives)
+
+                recall = true_positives / (true_positives + false_negatives)
+
+                specificity = true_negatives / (true_negatives + false_positives)
+
+                balanced_accuracy = (recall + specificity) / 2
+
+                mcc = ((true_positives * true_negatives) - (false_positives * false_negatives)) / np.sqrt((true_positives + false_positives) * (true_positives + false_negatives) * (true_negatives + false_positives) * (true_negatives + false_negatives))
+
+                f1_score = (2 * true_positives) / (2 * true_positives + false_positives + false_negatives)
+
+                metrics_list.append([accuracy, precision, recall, specificity, balanced_accuracy, mcc, f1_score])
+            
+            metrics_df = pd.DataFrame(metrics_list, index=conf_df.index, columns=columns_metrics)
+            
+            metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_9.csv')
+
+            return metrics_df, conf_df
+    
+    else:
+        for i, row in conf_df.iterrows():
+            true_positives = row['true_positives']
+            true_negatives = row['true_negatives']
+            false_positives = row['false_positives']
+            false_negatives = row['false_negatives']
+
+            accuracy = (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
+        
+            precision = true_positives / (true_positives + false_positives)
+
+            recall = true_positives / (true_positives + false_negatives)
+
+            specificity = true_negatives / (true_negatives + false_positives)
+
+            balanced_accuracy = (recall + specificity) / 2
+
+            mcc = ((true_positives * true_negatives) - (false_positives * false_negatives)) / np.sqrt((true_positives + false_positives) * (true_positives + false_negatives) * (true_negatives + false_positives) * (true_negatives + false_negatives))
+
+            f1_score = (2 * true_positives) / (2 * true_positives + false_positives + false_negatives)
+
+            metrics_list.append([accuracy, precision, recall, specificity, balanced_accuracy, mcc, f1_score])
+        
+        metrics_df = pd.DataFrame(metrics_list, index=conf_df.index, columns=columns_metrics)
+        
+        metrics_df.to_csv(cur + '\\data_team_1\\metrics\\metrics_9.csv')
+
+        return metrics_df, conf_df
+
+
+def plot_metrics_8(metrics_df):
+    col= list(metrics_df.columns)
+    x = np.arange(metrics_df.shape[0])
+    c = np.random.rand(metrics_df.shape[0],3)
+    for i in col:
+        fig, ax = plt.subplots(figsize=(20,10))
+        ax.bar(metrics_df.index,list(metrics_df[i]), color = c)
+        ax.set_xticks(x)
+        ax.set_xticklabels(metrics_df.index, rotation=65)
+        ax.set_ylim((0,1))
+        plt.title(i)
+        #fig.savefig(i+".png")
